a46932e3e4eb477f3df697e87c215b3214a510a0
Ravi Gehlot <ravi@gehlot.net>
2022-05-27 11:37:53 -0400

    retesting github function

--- netlify/functions/github-api/github-api.js
+++ netlify/functions/github-api/github-api.js
@@ -2,10 +2,10 @@ import { Octokit, App } from "octokit";
 
 const handler = async (event) => {
   const octokit = new Octokit({
-    auth: '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'
+    auth: 'ravigehlot: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'
   })
 
-  let result = await octokit.request('GET /user', {})
+  let result = await octokit.request('GET /users/ravigehlot', {})
 
   return result;
 }
